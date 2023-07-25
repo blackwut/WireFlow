b050625d0b25ed82d240f0b8c5df179446f28c54
blackwut <albertoottimo@gmail.com>
2023-07-25 16:27:55 +0200

    Removed unused import

--- FSP/__init__.py
+++ FSP/__init__.py
@@ -4,4 +4,3 @@ from .fbuffer import FBuffer, FBufferAccess
 from .fchannel import FChannel
 from .foperator import FOperator, FOperatorKind
 from .fapplication import FApplication, FTransferMode, FTarget
-from .fgeneratorintel import FGeneratorIntel
