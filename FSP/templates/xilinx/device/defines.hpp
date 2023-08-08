868bf9e689b3743da84adb65b474b310e842a1aa
blackwut <albertoottimo@gmail.com>
2023-08-08 17:46:09 +0200

    Renaming and cleaning

--- FSP/templates/xilinx/device/defines.hpp
+++ FSP/templates/xilinx/device/defines.hpp
@@ -4,13 +4,15 @@
 {% endfor %}
 {%- endmacro %}
 
-{%- macro define_axis_stream(mr, mw) %}
+{%- macro define_axis_streams(mr, mw) %}
 using mr_axis_stream_t = fx::axis_stream<{{mr.o_datatype}}, 16>;
 using mw_axis_stream_t = fx::axis_stream<{{mw.o_datatype}}, 16>;
 {%- endmacro %}
 
-{%- macro define_stream(datatype) %}
-using stream_{{datatype}} = fx::stream<{{datatype}}, 16>;
+{%- macro define_streams(tuples) %}
+{% for t in tuples %}
+using stream_{{t}} = fx::stream<{{t}}, 16>;
+{% endfor %}
 {%- endmacro %}
 
 #ifndef __DEFINES_HPP__
@@ -22,9 +24,7 @@ using stream_{{datatype}} = fx::stream<{{datatype}}, 16>;
 static constexpr int BUS_WIDTH = 512;
 using line_t = ap_uint<BUS_WIDTH>;
 
-{{define_axis_stream(mr, mw)}}
-{% for dt in datatypes %}
-{{define_stream(dt)}}
-{% endfor %}
+{{define_axis_streams(mr, mw)}}
+{{define_streams(tuples)}}
 
 #endif // __DEFINES_HPP__
