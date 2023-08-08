2c5fea06468e723f16763ba7187eb8d7f43a1b21
blackwut <albertoottimo@gmail.com>
2023-08-08 17:47:05 +0200

    Updated to target Xilinx

--- SpikeDetection/sd.py
+++ SpikeDetection/sd.py
@@ -16,44 +16,45 @@ def next_power_of_two(n):
     return n << 1
 
 
-source_par = 1
+mr_par = 1
 avg_par = 1
 spike_par = 1
-sink_par = 1
+mw_par = 1
 
-transfer_t = FTransferMode.COPY   # COPY, HYBRID or SHARED
+transfer_t = FTransferMode.COPY     # COPY, HYBRID or SHARED
 benchmark_t = 'throughput'          # latency or throughput
 precision_t = 'float'               # float or double
+in_datatype_t = ('input_t' if benchmark_t == 'throughput' else 'tuple_t')
 
 transfer_char = ('s' if transfer_t == FTransferMode.SHARED else 'c')
 benchmark_char = ('t' if benchmark_t == 'throughput' else 'l')
 precision_char = ('f' if precision_t == 'float' else 'd')
 
-source_node = FOperator('source',
-                    source_par,
-                    FOperatorKind.SOURCE,
+mr_node = FOperator('mr',
+                    mr_par,
+                    FOperatorKind.MEMORY_READER,
                     FGatherPolicy.NONE,
                     FDispatchPolicy.KB)
 
 avg_node = FOperator('average_calculator',
-                 avg_par,
-                 FOperatorKind.MAP,
-                 FGatherPolicy.LB,
-                 FDispatchPolicy.RR,
-                 begin_function=True,
-                 o_datatype='tuple_t')
+                     avg_par,
+                     FOperatorKind.MAP,
+                     FGatherPolicy.LB,
+                     FDispatchPolicy.RR,
+                     begin_function=True,
+                     o_datatype='tuple_t')
 
 spike_node = FOperator('spike_detector',
-                   spike_par,
-                   FOperatorKind.FILTER,
-                   FGatherPolicy.LB,
-                   FDispatchPolicy.LB)
+                       spike_par,
+                       FOperatorKind.FILTER,
+                       FGatherPolicy.LB,
+                       FDispatchPolicy.LB)
 
-sink_node = FOperator('sink',
-                  sink_par,
-                  FOperatorKind.SINK,
-                  FGatherPolicy.LB,
-                  FDispatchPolicy.NONE)
+mw_node = FOperator('mw',
+                    mw_par,
+                    FOperatorKind.MEMORY_WRITER,
+                    FGatherPolicy.LB,
+                    FDispatchPolicy.NONE)
 
 win_dim = 16                                       # window size
 max_keys = 64                                      # max n. of keys in total
@@ -75,22 +76,21 @@ avg_node.add_local_buffer(precision_t,
 pipe_folder = 'sd{}{}{}{:d}{:d}{:d}{:d}'.format(transfer_char,
                                                 benchmark_char,
                                                 precision_char,
-                                                source_par,
+                                                mr_par,
                                                 avg_par,
                                                 spike_par,
-                                                sink_par)
+                                                mw_par)
 
 pipe = FApplication(pipe_folder,
-             ('input_t' if benchmark_t == 'throughput' else 'tuple_t'),
-             target = FTarget.INTEL,
-             constants=constants,
-             transfer_mode=transfer_t,
-             codebase="./codebase")
-pipe.add_source(source_node)
+                    in_datatype_t,
+                    target = FTarget.XILINX,
+                    constants=constants,
+                    transfer_mode=transfer_t,
+                    codebase="./codebase")
+pipe.add(mr_node)
 pipe.add(avg_node)
 pipe.add(spike_node)
-pipe.add_sink(sink_node)
+pipe.add(mw_node)
 
-# pipe.finalize()
 pipe.generate_device(rewrite=True)
 pipe.generate_host()
