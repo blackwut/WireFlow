9014b201adbd7f3f367ee769803ef7510db7de1e
blackwut <albertoottimo@gmail.com>
2023-12-07 17:11:46 +0100

    [Update] Fixes and cleaning

--- FSP/fapplication.py
+++ FSP/fapplication.py
@@ -604,7 +604,6 @@ class FGeneratorIntel:
         self.generate_device(rewrite, rewrite_device, rewrite_functions, rewirte_tuples)
         self.generate_host(rewrite, rewrite_host, rewrite_pipe)
 
-
 class FGeneratorXilinx:
 
     def __init__(self,
@@ -624,18 +623,14 @@ class FGeneratorXilinx:
         """
         app
         ├── common
-        ├── ocl
         ├── device
         │   └── includes
         │   └── nodes
         └── host
-            └── includes (unused)
-            └── metric   (unused)
+            └── includes
         """
         self.app.base_dir = self.app.dest_dir
         self.app.common_dir = path.join(self.app.base_dir, 'common')
-
-        self.app.ocl_dir = path.join(self.app.base_dir, 'ocl')
 
         self.app.device_dir = path.join(self.app.base_dir, 'device')
         self.app.device_includes_dir = path.join(self.app.device_dir, 'includes')
@@ -643,11 +638,10 @@ class FGeneratorXilinx:
 
         self.app.host_dir = path.join(self.app.base_dir, 'host')
         self.app.host_includes_dir = path.join(self.app.host_dir, 'includes')
-        self.app.host_metric_dir = path.join(self.app.host_dir, 'metric')
 
-        for folder in (self.app.base_dir, self.app.common_dir, self.app.ocl_dir,
+        for folder in (self.app.base_dir, self.app.common_dir,
                        self.app.device_dir, self.app.device_includes_dir, self.app.device_nodes_dir,
-                       self.app.host_dir, self.app.host_includes_dir, self.app.host_metric_dir):
+                       self.app.host_dir, self.app.host_includes_dir):
             if not path.isdir(folder):
                 os.mkdir(folder)
 
@@ -686,94 +680,117 @@ class FGeneratorXilinx:
 
         return tuples
 
-    def generate_constants(self,
-                           rewrite=False):
+    def generate_constants(self, rewrite=False):
         filename = 'constants.hpp'
-        template = read_template_file(self.app.dest_dir, filename, 'xilinx')
-        result = template.render(constants=self.app.constants | self.get_par_constants())
 
+        # if 'constants.hpp' is already present in codebase folder
+        if self.app.codebase:
+            codebase_filepath = path.join(self.app.codebase, 'includes', filename)
+            if rewrite and path.isfile(codebase_filepath):
+                copyfile(codebase_filepath, path.join(self.app.common_dir, filename))
+                return
+
+        # otherwise generate it
         filepath = path.join(self.app.common_dir, filename)
-        if not path.isfile(filepath) or rewrite:
+        if rewrite or not path.isfile(filepath):
+            template = read_template_file(self.app.dest_dir, filename, 'xilinx')
+            result = template.render(constants=self.app.constants | self.get_par_constants())
             file = open(filepath, mode='w+')
             file.write(result)
             file.close()
 
     def generate_keyby_lambdas(self, rewrite=False):
         filename = "keyby_lambdas.hpp"
-        template = read_template_file(self.app.dest_dir, filename, 'xilinx')
+
+        # if 'keyby_lambdas.hpp' is already present in codebase folder
+        if self.app.codebase:
+            codebase_filepath = path.join(self.app.codebase, 'includes', filename)
+            if rewrite and path.isfile(codebase_filepath):
+                copyfile(codebase_filepath, path.join(self.app.common_dir, filename))
+                return
+
+        # otherwise generate it
         filepath = path.join(self.app.device_includes_dir, filename)
-        if not path.isfile(filepath) or rewrite:
-            file = open(filepath, mode='w+')
+        if rewrite or not path.isfile(filepath):
+            template = read_template_file(self.app.dest_dir, filename, 'xilinx')
             result = template.render(nodes=self.app.get_nodes())
+            file = open(filepath, mode='w+')
             file.write(result)
             file.close()
 
-    def generate_tuples(self,
-                        rewrite=False):
-
-        template = read_template_file(self.app.dest_dir, 'tuple.hpp', 'xilinx')
+    def generate_tuples(self, rewrite=False):
         tuples = self.get_tuples()
-
+        template = read_template_file(self.app.dest_dir, 'tuple.hpp', 'xilinx')
         for tuple in tuples:
             filename = tuple + '.hpp'
             filepath = path.join(self.app.common_dir, filename)
-            # do not generate tuples if they are already present in codebase folder
+            # if filename of corresponding tuple is already present in codebase folder
             if self.app.codebase:
-                codebase_filepath = path.join(self.app.codebase, 'includes', filename)
-                if path.isfile(codebase_filepath):
+                codebase_filepath = path.join(self.app.codebase, 'common', filename)
+                if rewrite and path.isfile(codebase_filepath):
                     copyfile(codebase_filepath, filepath)
                     continue
 
-            # do not generate tuples if are already present or rewrite is false
-            if path.isfile(filepath) and not rewrite:
+            # otherwise generate it
+            if rewrite or not path.isfile(filepath):
+                result = template.render(tuple_name=tuple)
+                file = open(filepath, mode='w+')
+                file.write(result)
+                file.close()
+
+    def generate_functor_for(self, node, rewrite=False):
+        # check if node is not a generator, drainer, memory reader or memory writer
+        assert not node.is_generator() and not node.is_drainer() and not node.is_memory_reader() and not node.is_memory_writer()
+
+        filename = node.name + '.hpp'
+
+        # if filename of corresponding functor is already present in codebase folder
+        if self.app.codebase:
+            codebase_filepath = path.join(self.app.codebase, 'device', 'nodes', filename)
+            if rewrite and path.isfile(codebase_filepath):
+                copyfile(codebase_filepath, path.join(self.app.device_nodes_dir, filename))
                 return
 
+        # otherwise generate it
+        filepath = path.join(self.app.device_nodes_dir, filename)
+        if rewrite or not path.isfile(filepath):
+            template = read_template_file(self.app.dest_dir, 'operator.hpp', 'xilinx')
+            result = template.render(op=node)
             file = open(filepath, mode='w+')
-            result = template.render(tuple_name=tuple)
             file.write(result)
             file.close()
 
-    def generate_functor_for(self, node):
-        template = read_template_file(self.app.dest_dir, 'operator.hpp', 'xilinx')
-        filename = node.name + '.hpp'
-        filepath = path.join(self.app.device_nodes_dir, filename)
-
-        file = open(filepath, mode='w+')
-        result = template.render(op=node)
-        file.write(result)
-        file.close()
-
-    def generate_functions(self,
-                           rewrite=False):
+    def generate_functions(self, rewrite=False):
 
         for node in self.app.internal_nodes:
-            is_generated = False
-            filename = node.name + '.hpp'
-            filepath = path.join(self.app.device_nodes_dir, filename)
+            self.generate_functor_for(node, rewrite)
+            # is_generated = False
+            # filename = node.name + '.hpp'
+            # filepath = path.join(self.app.device_nodes_dir, filename)
 
-            if self.app.codebase:
-                codebase_filepath = path.join(self.app.codebase, 'device', 'nodes', filename)
-                if path.isfile(codebase_filepath):
-                    copyfile(codebase_filepath, filepath)
-                    is_generated = True
+            # if self.app.codebase:
+            #     codebase_filepath = path.join(self.app.codebase, 'device', 'nodes', filename)
+            #     if rewrite and path.isfile(codebase_filepath):
+            #         copyfile(codebase_filepath, filepath)
+            #         is_generated = True
 
-            if path.isfile(filepath) and not rewrite:
-                is_generated = True
+            # if not rewrite and path.isfile(filepath):
+            #     is_generated = True
 
-            if not is_generated:
-                self.generate_functor_for(node)
+            # if not is_generated:
+            #     self.generate_functor_for(node, rewrite)
 
     def generate_makefile(self, rewrite=False):
-        template = read_template_file(self.app.dest_dir, 'Makefile', 'xilinx')
         filepath = path.join(self.app.base_dir, 'Makefile')
-        if not path.isfile(filepath) or rewrite:
-            file = open(filepath, mode='w+')
+        if rewrite or not path.isfile(filepath):
+            template = read_template_file(self.app.dest_dir, 'Makefile', 'xilinx')
             result = template.render(app_name=self.app.dest_dir,
                                      operators=self.app.get_nodes(),
                                      mr=self.app.memory_reader,
                                      mw=self.app.memory_writer,
                                      transfer_mode=self.app.transfer_mode,
                                      transferMode=FTransferMode)
+            file = open(filepath, mode='w+')
             file.write(result)
             file.close()
 
@@ -785,44 +802,43 @@ class FGeneratorXilinx:
 ################################################################################
 
     def generate_defines(self, rewrite=False):
-        template = read_template_file(self.app.dest_dir, 'defines.hpp', 'xilinx')
         filename = path.join(self.app.device_includes_dir, 'defines.hpp')
-
-        if not path.isfile(filename) or rewrite:
-            file = open(filename, mode='w+')
+        if rewrite or not path.isfile(filename):
+            template = read_template_file(self.app.dest_dir, 'defines.hpp', 'xilinx')
             result = template.render(tuples=self.get_tuples(),
                                      mr=self.app.memory_reader,
                                      mw=self.app.memory_writer)
+            file = open(filename, mode='w+')
             file.write(result)
             file.close()
 
     def generate_mr_kernel(self, rewrite=False):
-        template = read_template_file(self.app.dest_dir, 'memory_reader.cpp', 'xilinx')
         filename = path.join(self.app.device_dir, 'memory_reader.cpp')
-        if not path.isfile(filename) or rewrite:
-            file = open(filename, mode='w+')
+        if rewrite or not path.isfile(filename):
+            template = read_template_file(self.app.dest_dir, 'memory_reader.cpp', 'xilinx')
             result = template.render()
+            file = open(filename, mode='w+')
             file.write(result)
             file.close()
 
     def generate_compute_kernel(self, rewrite=False):
-        template = read_template_file(self.app.dest_dir, 'compute.cpp', 'xilinx')
         filename = path.join(self.app.device_dir, 'compute.cpp')
-        if not path.isfile(filename) or rewrite:
-            file = open(filename, mode='w+')
+        if rewrite or not path.isfile(filename):
+            template = read_template_file(self.app.dest_dir, 'compute.cpp', 'xilinx')
             result = template.render(operators=self.app.internal_nodes,
                                      nodes=self.app.get_nodes(),
                                      mr=self.app.memory_reader,
                                      mw=self.app.memory_writer)
+            file = open(filename, mode='w+')
             file.write(result)
             file.close()
 
     def generate_mw_kernel(self, rewrite=False):
-        template = read_template_file(self.app.dest_dir, 'memory_writer.cpp', 'xilinx')
         filename = path.join(self.app.device_dir, 'memory_writer.cpp')
-        if not path.isfile(filename) or rewrite:
-            file = open(filename, mode='w+')
+        if rewrite or not path.isfile(filename):
+            template = read_template_file(self.app.dest_dir, 'memory_writer.cpp', 'xilinx')
             result = template.render()
+            file = open(filename, mode='w+')
             file.write(result)
             file.close()
 
@@ -837,14 +853,30 @@ class FGeneratorXilinx:
 #
 ################################################################################
 
+    def generate_host_includes(self, rewrite=False):
+        if self.app.codebase:
+            codebase_includes_dir = path.join(self.app.codebase, 'host', 'includes')
+            for f in os.listdir(codebase_includes_dir):
+                src_path = path.join(codebase_includes_dir, f)
+                dest_path = path.join(self.app.host_includes_dir, f)
+                if rewrite or not path.isfile(dest_path):
+                    copyfile(src_path, dest_path)
+
+
     def generate_main(self, rewrite=False):
-        template = read_template_file(self.app.dest_dir, 'host.cpp', 'xilinx')
         filename = path.join(self.app.host_dir, 'host.cpp')
-        if not path.isfile(filename) or rewrite:
-            file = open(filename, mode='w+')
+        if self.app.codebase:
+            codebase_filepath = path.join(self.app.codebase, 'host', 'host.cpp')
+            if rewrite and path.isfile(codebase_filepath):
+                copyfile(codebase_filepath, filename)
+                return
+
+        if rewrite or not path.isfile(filename):
+            template = read_template_file(self.app.dest_dir, 'host.cpp', 'xilinx')
             result = template.render(tuples=self.get_tuples(),
                                      mr=self.app.memory_reader,
                                      mw=self.app.memory_writer)
+            file = open(filename, mode='w+')
             file.write(result)
             file.close()
 
@@ -885,17 +917,20 @@ class FGeneratorXilinx:
     def generate_host(self,
                       rewrite=False,
                       rewrite_host=False,
+                      rewrite_host_includes=False,
                       rewrite_pipe=False):
 
         assert self.app.transfer_mode != FTransferMode.SHARED
 
         rewrite_host = rewrite or rewrite_host
+        rewrite_host_includes = rewrite or rewrite_host_includes
         rewrite_pipe = rewrite or rewrite_pipe
 
         self.app.finalize()
         self.prepare_folders()
 
         self.generate_constants(rewrite)
+        self.generate_host_includes(rewrite_host_includes)
         self.generate_main(rewrite_host)
 
     def generate_code(self,
@@ -904,7 +939,8 @@ class FGeneratorXilinx:
                       rewrite_functions=False,
                       rewirte_tuples=False,
                       rewrite_host=False,
+                      rewrite_host_includes=False,
                       rewrite_pipe=False,
                       rewrite_keyby_lambdas=False):
         self.generate_device(rewrite, rewrite_device, rewrite_functions, rewirte_tuples, rewrite_keyby_lambdas)
-        self.generate_host(rewrite, rewrite_host, rewrite_pipe)
+        self.generate_host(rewrite, rewrite_host, rewrite_host_includes, rewrite_pipe)
--- FSP/templates/xilinx/common/constants.hpp
+++ FSP/templates/xilinx/common/constants.hpp
@@ -2,7 +2,9 @@
 #define __CONSTANTS_HPP__
 
 {% for key, value in constants.items() %}
+#ifndef {{ key | upper }}
 #define {{ '%-50s' | format((key | upper)) + (value | string) }}
+#endif // {{ key | upper }}
 {% endfor %}
 
 #endif // __CONSTANTS_HPP__
