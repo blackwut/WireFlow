e799a729b06c019efef4f8debefa15d132f14301
blackwut <albertoottimo@gmail.com>
2023-12-12 15:24:23 +0100

    [Update] added commonheader files

---
+++ Applications/common/benchmark.hpp
@@ -0,0 +1,122 @@
+#ifndef __BENCHMARK_HPP__
+#define __BENCHMARK_HPP__
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "fspx_host.hpp"
+
+size_t get_tuples_sec(size_t tuples, uint64_t time)
+{
+    return tuples / (time / 1e9);
+}
+
+size_t get_bytes_sec(size_t tuples, size_t tuple_size, uint64_t time)
+{
+    return get_tuples_sec(tuples, time) * tuple_size;
+}
+
+void dump_benchmark(
+    std::string filename,
+    std::string appname,
+    std::string bitstream,
+    std::vector<std::string> operator_names,
+    std::vector<size_t> operator_parallelisms,
+    size_t mr_num_buffers,
+    size_t mr_batch_size,
+    size_t mw_num_buffers,
+    size_t mw_batch_size,
+    size_t sampling_rate,
+    size_t tuple_sent_size,
+    size_t tuples_sent,
+    size_t batches_sent,
+    size_t tuple_received_size,
+    size_t tuples_received,
+    size_t batches_received,
+    uint64_t elapsed_time_host,
+    uint64_t elapsed_time_compute)
+{
+    bool print_header = false;
+    std::ifstream infile(filename);
+    if (!infile.good()) {
+        print_header = true;
+    }
+    infile.close();
+
+    std::ofstream outfile(filename, std::ios_base::app);
+    if (print_header) {
+        outfile << "Application" << ","
+                << "Bitstream" << ",";
+
+        for (auto name : operator_names) {
+            outfile << name << ",";
+        }
+
+        outfile << "MR Buffers" << ","
+                << "MW Buffers" << ","
+                << "MR BatchSize" << ","
+                << "MW BatchSize" << ","
+                << "Sampling" << ","
+                << "TupleSent" << ","
+                << "BatchesSent" << ","
+                << "TuplesReceived" << ","
+                << "BatchesReceived" << ","
+                << "Throughput MR (t/s)" << ","
+                << "Throughput MR (B/s)" << ","
+                << "Throughput MW (t/s)" << ","
+                << "Throughput MW (B/s)" << ","
+                << "Time (ms)" << ","
+                << "Time Compute (ns)" << ","
+                << "Samples" << ","
+                << "Total" << ","
+                << "Mean" << ","
+                << "0" << ","
+                << "5" << ","
+                << "25" << ","
+                << "50" << ","
+                << "75" << ","
+                << "95" << ","
+                << "100"
+                << std::endl;
+    }
+
+    auto latency_metric = fx::metric_group.get_metric("latency");
+
+    outfile << appname << ","
+            << bitstream << ",";
+
+    for (auto par : operator_parallelisms) {
+        outfile << par << ",";
+    }
+
+    outfile << mr_num_buffers << ","
+            << mw_num_buffers << ","
+            << mr_batch_size << ","
+            << mw_batch_size << ","
+            << sampling_rate << ","
+            << tuples_sent << ","
+            << batches_sent << ","
+            << tuples_received << ","
+            << batches_received << ","
+            << get_tuples_sec(tuples_sent, elapsed_time_compute) << ","
+            << get_bytes_sec(tuples_sent, elapsed_time_compute) << ","
+            << get_tuples_sec(tuples_received, tuple_sent_size, elapsed_time_compute) << ","
+            << get_bytes_sec(tuples_received, tuple_received_size, elapsed_time_compute) << ","
+            << elapsed_time_host << ","
+            << elapsed_time_compute << ","
+            << latency_metric.getN() << ","
+            << latency_metric.total() << ","
+            << (uint64_t)(latency_metric.mean() / 1e3) << ","
+            << (uint64_t)(latency_metric.min() / 1e3) << ","
+            << (uint64_t)(latency_metric.percentile(0.05) / 1e3) << ","
+            << (uint64_t)(latency_metric.percentile(0.25) / 1e3) << ","
+            << (uint64_t)(latency_metric.percentile(0.5) / 1e3) << ","
+            << (uint64_t)(latency_metric.percentile(0.75) / 1e3) << ","
+            << (uint64_t)(latency_metric.percentile(0.95) / 1e3) << ","
+            << (uint64_t)(latency_metric.max() / 1e3)
+            << std::endl;
+    outfile.close();
+}
+
+#endif // __BENCHMARK_HPP__
---
+++ Applications/common/drainer_thread.hpp
@@ -0,0 +1,61 @@
+#ifndef __STREAM_DRAINER_THREAD_HPP__
+#define __STREAM_DRAINER_THREAD_HPP__
+
+#include <atomic>
+#include <vector>
+#include <pthread.h>
+
+#include "fspx_host.hpp"
+
+extern pthread_barrier_t barrier_setup;
+extern pthread_barrier_t barrier_start;
+extern pthread_barrier_t barrier_end;
+
+extern std::atomic<size_t> tuples_received;
+extern std::atomic<size_t> batches_received;
+
+
+template <typename T>
+void stream_drainer_thread(
+    const size_t idx,
+    fx::OCL & ocl,
+    const size_t num_batches,
+    const size_t batch_size,
+    const uint64_t & app_start_time,
+    const size_t sampling_rate
+)
+{
+    fx::StreamDrainer<T> stream_drainer(ocl, batch_size, num_batches, idx);
+    size_t _tuples_received = 0;
+    size_t _batch_received = 0;
+
+    fx::Sampler latency_sampler(sampling_rate);
+
+    stream_drainer.prelaunch();
+    pthread_barrier_wait(&barrier_setup);
+    pthread_barrier_wait(&barrier_start);
+
+    bool done = false;
+    while(!done) {
+        size_t items_written = 0;
+        T * batch = stream_drainer.pop(&items_written, &done);
+
+        const uint64_t current_time = fx::current_time_nsecs();
+        for (size_t i = 0; i < items_written; ++i) {
+            const double latency = (current_time - app_start_time) - batch[i].timestamp * uint64_t(100);
+            latency_sampler.add(latency, current_time);
+        }
+
+        stream_drainer.put_batch(batch, batch_size);
+        _tuples_received += items_written;
+        _batch_received++;
+    }
+    stream_drainer.finish();
+    pthread_barrier_wait(&barrier_end);
+
+    tuples_received += _tuples_received;
+    batches_received += _batch_received;
+    fx::metric_group.add("latency", latency_sampler);
+}
+
+#endif // __STREAM_DRAINER_THREAD_HPP__
